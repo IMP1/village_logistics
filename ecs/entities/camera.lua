0bf17bba4d80734f8dfe4a5a5c3a342f84a1cbd3
IMP1 <IMP1@mail.com>
2019-03-26 13:42:54 +0000

    camera entity

---
+++ ecs/entities/camera.lua
@@ -0,0 +1,16 @@
+
+local name = "camera_2d"
+
+return {
+    name = name,
+    components = {
+        viewport = {
+            bounds = {0, 0, love.graphics.getWidth(), love.graphics.getHeight()},
+        },
+        transform = {
+            translation = {0, 0},
+            scale       = 1,
+            rotation    = 0,
+        },
+    }
+}
