107bb7b675f9cd532baee1143bec918220f7809c
Huw Taylor <huw_taylor@hotmail.co.uk>
2019-04-06 00:19:28 +0100

    added drawing of shapes

--- ecs/systems/renderer.lua
+++ ecs/systems/renderer.lua
@@ -2,6 +2,11 @@ local entity_manager = require 'ecs.entity_manager'
 
 local name = "renderer"
 
+local world_filter   = entity_manager.component_filter("location", "renderable")
+local overlay_filter = entity_manager.component_filter("location", "gui")
+local gui_filter     = entity_manager.component_filter("viewport", "gui")
+local camera_filter  = entity_manager.component_filter("viewport", "transform")
+
 local function draw_renderable(entity)
     local visible = entity.components.renderable.visible
     if not visible then return end
@@ -9,19 +14,29 @@ local function draw_renderable(entity)
     local ox, oy = unpack(entity.components.renderable.offset or {0, 0})
     local r = 0 -- TODO: get these from somewhere
     local sx, sx = 1, 1
-    local texture = entity.components.renderable.texture
     local colour = entity.components.renderable.colour
-    local quad = entity.components.renderable.quad
-    love.graphics.setColor(colour)
-    if quad then
-        love.graphics.draw(texture, quad, x, y, r, sx, sy, ox, oy)
-    else
-        love.graphics.draw(texture, x, y, r, sx,sy, ox, oy)
+    local texture = entity.components.renderable.texture
+    local shape = entity.components.renderable.shape
+    
+    if texture then
+        love.graphics.setColor(colour)
+        local quad = entity.components.renderable.quad
+        if quad then
+            love.graphics.draw(texture, quad, x, y, r, sx, sy, ox, oy)
+        else
+            love.graphics.draw(texture, x, y, r, sx,sy, ox, oy)
+        end
+    elseif shape then
+        local points = entity.components.renderable.shape.points
+        local fill_colour = entity.components.renderable.fill_colour
+        if fill_colour then
+            love.graphics.setColor(fill_colour)
+            love.graphics.polygon("fill", points)
+        end
+        love.graphics.setColor(colour)
+        love.graphics.polygon("line", points)
     end
 end
-
-local world_filter = entity_manager.component_filter("location", "renderable")
-local camera_filter = entity_manager.component_filter("viewport", "transform")
 
 local function draw_world(camera)
     love.graphics.push()
@@ -34,14 +49,22 @@ local function draw_world(camera)
     for _, entity in pairs(entity_manager.get_entities(world_filter)) do
         draw_renderable(entity)
     end
+    for _, entity in pairs(entity_manager.get_entities(overlay_filter)) do
+        draw_renderable(entity)
+    end
 
     love.graphics.setScissor()
     love.graphics.pop()
 end
 
--- local gui_filter = entity_manager.component_filter("")
+local function draw_gui_element(entity)
+    -- TODO: draw GUI elements
+end
+
 local function draw_gui()
--- TODO: draw GUI elements
+    for _, entity in pairs(entity_manager.get_entities(gui_filter)) do
+        draw_gui_element(entity)
+    end
 end
 
 local function draw(system)
