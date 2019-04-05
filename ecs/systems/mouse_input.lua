2be43a450b03d65e5fac1be0d98b4d44e6057074
Huw Taylor <huw_taylor@hotmail.co.uk>
2019-04-06 00:19:57 +0100

    added movement of mouse as event and selection box is now drawn

--- ecs/systems/mouse_input.lua
+++ ecs/systems/mouse_input.lua
@@ -6,12 +6,14 @@ local name = "mouse_input"
 local filter = entity_manager.component_filter("transform")
 
 local mouse_movement = {0, 0}
+local mouse_down     = love.mouse.isDown(1)
 
 local function mousedown(system, entity, mx, my, button)
     local ox, oy = unpack(entity.components.transform.translation)
     if button == 1 then
         mouse_movement = {mx + ox, my + oy}
     end
+    mouse_down = true
 end
 
 local function mouseup(system, entity, mx, my, button)
@@ -24,10 +26,19 @@ local function mouseup(system, entity, mx, my, button)
         dy = wy - mouse_movement[2]
     end
     if dx ~= 0 or dy ~= 0 then
-        system_manager.broadcast("ondrag", mx, my, dx, dy, wx, wy)
+        system_manager.broadcast("ondrag", wx, wy, dx, dy, mx, my)
     else
-        system_manager.broadcast("onclick", mx, my, button, wx, wy)
+        system_manager.broadcast("onclick", wx, wy, button, mx, my)
     end
+    mouse_down = false
+end
+
+local function mousemoved(system, entity, mx, my, dx, dy)
+    local ox, oy = unpack(entity.components.transform.translation)
+    local wx = mx + ox
+    local wy = my + oy
+    local x, y = unpack(mouse_movement)
+    system_manager.broadcast("onpan", wx, wy, dx, dy, mouse_down, x, y, mx, my)
 end
 
 return {
@@ -35,9 +46,11 @@ return {
     filters = { 
         mousepressed  = filter,
         mousereleased = filter,
+        mousemoved    = filter,
     },
     events  = { 
         mousepressed  = mousedown,
         mousereleased = mouseup,
+        mousemoved    = mousemoved,
     },
 }
--- ecs/systems/selection.lua
+++ ecs/systems/selection.lua
@@ -4,6 +4,7 @@ local system_manager = require 'ecs.system_manager'
 local name = "unit_selection"
 
 local selected_entities = {}
+local drag_rectangle = nil
 
 local filter = entity_manager.component_filter("selectable", "location")
 
@@ -13,7 +14,11 @@ local function is_over(x, y, mx, my, leeway)
     return dx*dx + dy*dy <= leeway*leeway
 end
 
-local function click(system, mx, my, button, wx, wy)
+local function is_in(x, y, ox, oy, w, h)
+    return x >= ox and y >= oy and x <= ox + w and y <= oy + h
+end
+
+local function click(system, wx, wy, button)
     local selection = {}
     for _, entity in pairs(entity_manager.get_entities(filter)) do
         local x, y = unpack(entity.components.location.position)
@@ -30,15 +35,61 @@ local function click(system, mx, my, button, wx, wy)
                 selection = {}
                 table.insert(selection, entity)
             end
-            -- TODO: check priority + multiple
         end
     end
-    print(#selection, "selected items.")
+    if #selection > 0 then
+        selected_entities = selection
+    end
 end
 
-local function drag(system, mx, my, dx, dy, wx, wy)
-    -- TODO: get entities in square that are selectable.
-    print(wx, wy, dx, dy)
+local function drag(system, wx, wy, dx, dy)
+    local selection = {}
+    for _, entity in pairs(entity_manager.get_entities(filter)) do
+        local x, y = unpack(entity.components.location.position)
+        local ox, oy = unpack(entity.components.selectable.offset or {0, 0})
+        local priority = entity.components.selectable.priority
+        if is_in(x - ox, y - oy, wx-dx, wy-dy, dx, dy) then
+            if #selection == 0 then
+                table.insert(selection, entity)
+            elseif selection[1].components.selectable.multiple and
+                   priority == selection[1].components.selectable.priority then
+                table.insert(selection, entity)
+            elseif priority > selection[1].components.selectable.priority then
+                selection = {}
+                table.insert(selection, entity)
+            end
+        end
+    end
+    if #selection > 0 then
+        selected_entities = selection
+    end
+end
+
+local function move(system, wx, wy, dx, dy, mouse_down, ox, oy)
+    if not mouse_down then
+        if drag_rectangle then
+            entity_manager.delete_entity(drag_rectangle)
+        end
+        drag_rectangle = nil
+        return
+    end
+    if not drag_rectangle then
+        drag_rectangle = entity_manager.create_entity("drag_rectangle")
+        entity_manager.add_component(drag_rectangle, "location", {
+            position = {}
+        })
+        entity_manager.add_component(drag_rectangle, "renderable", {
+            visible = true,
+            colour  = {1, 1, 0},
+            shape   = {
+                points = {0, 0, 0, 0, 0, 0},
+            },
+        })
+    end
+    local x1, x2 = math.min(ox, wx), math.max(ox, wx)
+    local y1, y2 = math.min(oy, wy), math.max(oy, wy)
+    local shape = entity_manager.get_component(drag_rectangle, "renderable").shape
+    shape.points = {x1, y1, x2, y1, x2, y2, x1, y2}
 end
 
 return {
@@ -46,9 +97,11 @@ return {
     filters = { 
         onclick = entity_manager.filter_none,
         ondrag  = entity_manager.filter_none,
+        onpan   = entity_manager.filter_none,
     },
     events  = { 
         onclick = click,
         ondrag  = drag,
+        onpan   = move,
     },
 }
