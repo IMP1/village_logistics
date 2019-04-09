f0cbc1571f743b4adf557dc9ef50d493e18b8b84
IMP1 <IMP1@mail.com>
2019-04-09 23:35:34 +0100

    added example of non-person worker to work out what harvesters can do

---
+++ ecs/entities/cart.lua
@@ -0,0 +1,30 @@
+local name = "cart"
+
+return {
+    name = name,
+    components = {
+        selectable = {
+            multiple = true,
+            priority = 10,
+            size     = 32,       -- radius around location
+            offset   = {16, 24}, -- from location in world
+        },
+        carrier = {
+            -- resource limits?
+        },
+        moveable = {
+            -- speed
+            -- path
+        },
+        location = {
+            position = {100, 100, 1},
+        },
+        renderable = {
+            visible = true,
+            colour  = {1, 1, 1},
+            texture = love.graphics.newImage("res/gfx/worker_male.png"), -- @TODO: change to a cart
+            quad    = love.graphics.newQuad(32, 0, 32, 48, 96, 192),
+            offset  = {16, 48},
+        },
+    }
+}
