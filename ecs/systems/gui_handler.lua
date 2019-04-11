3c3bf52de2ccd2e776188da41eceb98a238387ee
IMP1 <IMP1@mail.com>
2019-04-11 15:47:36 +0100

    added handler to pass clicks to gui entities

---
+++ ecs/systems/gui_handler.lua
@@ -0,0 +1,27 @@
+local entity_manager = require 'ecs.entity_manager'
+
+local name = "gui_handler"
+
+local filter = function(x, y)
+    return function(entity)
+        if entity.components.gui then
+            return entity.components.gui.is_over(entity, x, y)
+        end
+        return false
+    end
+end
+
+local function click(system, wx, wy)
+    for _, entity in pairs(entity_manager.get_entities(filter(wx, wy))) do
+        local gui = entity.components.gui
+        if not gui.disabled and gui.click then
+            gui.click(entity)
+        end
+    end
+end
+
+return {
+    name    = name,
+    filters = { onclick = entity_manager.filter_none },
+    events  = { onclick = click },
+}
