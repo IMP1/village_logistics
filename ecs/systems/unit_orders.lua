f7403e7868c69e893fc5b3818864e9002873010a
IMP1 <IMP1@mail.com>
2019-04-12 14:17:40 +0100

    generate harvst+carry command; only one unit selectable at a time

--- ecs/systems/unit_orders.lua
+++ ecs/systems/unit_orders.lua
@@ -3,38 +3,32 @@ local system_manager = require 'ecs.system_manager'
 
 local name = "unit_orders"
 
-local selected_units    = {}
+local selected_unit     = nil
 local avaialble_actions = {}
 
-local function selection(system, newly_selected_entities)
-    selected_units    = newly_selected_entities
+-- @BUG: clicking on resource created button and then also triggers button press. somehow.
+
+local function selection(system, newly_selected_entity)
+    selected_unit = newly_selected_entity
     for _, action in pairs(avaialble_actions) do
         entity_manager.delete_entity(action.button)
     end
     avaialble_actions = {}
 end
 
--- @NOTE: a lot of this gets much easier if you can't select more than one worker.
---        how great is the benefit to the player for that? when/how often do you
---        want more than one worker doing the same job?
-
--- @TODO: change to be one selected_unit
-
 local function get_options()
     local harvest = true
     local carry   = true
     local produce = true
-    for _, unit in pairs(selected_units) do
-        for k, v in pairs(unit.components) do print(k) end
-        if not unit.components.harvester then
-            harvest = false
-        end
-        if not unit.components.carrier then
-            carry = false
-        end
-        if not unit.components.producer then
-            produce = false
-        end
+    for k, v in pairs(selected_unit.components) do print(k) end
+    if not selected_unit.components.harvester then
+        harvest = false
+    end
+    if not selected_unit.components.carrier then
+        carry = false
+    end
+    if not selected_unit.components.producer then
+        produce = false
     end
     return {
         harvest = harvest,
@@ -47,9 +41,15 @@ local function get_filter_for_click(wx, wy, leeway)
     return function(entity)
         if not entity.components.location then return false end
         local x, y = unpack(entity.components.location.position)
+        local r = leeway or 16
+        if entity.components.selectable then
+            local ox, oy = unpack(entity.components.selectable.offset)
+            x = x - ox
+            y = y - oy
+            r = entity.components.selectable.size
+        end
         local dx = x - wx
         local dy = y - wy
-        local r = leeway or 16
         return dx*dx + dy*dy <= r*r
     end
 end
@@ -61,11 +61,18 @@ local function create_command_options(wx, wy)
     for _, entity in pairs(entity_manager.get_entities(get_filter_for_click(wx, wy))) do
         if possible_actions.harvest and entity.components.harvestable then
             table.insert(possible_commands, {object = entity, action = "harvest"})
+            if possible_actions.carry then
+                local resource = entity_manager.load_blueprint(entity.components.harvestable.resource)
+                local carrier = selected_unit.components.carrier
+                if carrier and carrier.max_weight >= resource.components.resource.unit_mass then
+                    table.insert(possible_commands, {object = entity, action = "harvest-and-carry"})
+                end
+            end
         end
         if possible_actions.carry and entity.components.resource then
             table.insert(possible_commands, {object = entity, action = "carry"})
         end
-        if possible_actions.produce and entity.components.producer then
+        if possible_actions.produce and entity.components.production then
             table.insert(possible_commands, {object = entity, action = "produce"})
         end
     end
@@ -73,11 +80,7 @@ local function create_command_options(wx, wy)
     print(#possible_commands, "possible commands.")
 
     if #possible_commands > 0 then
-        for _, cmd in pairs(possible_commands) do
-            print(cmd.action, cmd.object.name)
-            local x, y = unpack(cmd.object.components.location.position)
-            local button = entity_manager.create_entity("button")
-
+        for i, cmd in pairs(possible_commands) do
             local action
             if cmd.action == "produce" then
                 action = function(gui_entity)
@@ -87,16 +90,21 @@ local function create_command_options(wx, wy)
                 action = function(gui_entity)
                     -- @TODO: have next clicked place be target of carrying
                 end
+            elseif cmd.action == "harvest-and-carry" then
+                action = function(gui_entity)
+                    -- @TODO: have next clicked place be target of carrying
+                end
             elseif cmd.action == "harvest" then
                 action = function(gui_entity)
-                    local resource = entity_manager.load_blueprint(cmd.object.components.harvestable.resource)
-                    print("mass of " .. resource.name .. " is " .. resource.components.resource.unit_mass .. "kg.")
-                    if 
+
                     -- @TODO: need to check if resource that would be created by harvesting can be carried.
                     --        and if it if can, and the worker can also carry, have both options of just 
                     --        harvesting in place, or harvest and carry.
                 end
             end
+
+            local x, y = unpack(cmd.object.components.location.position)
+            local button = entity_manager.create_entity("button")
 
             entity_manager.add_component(button, "location", {
                 position = {x-10, y - 32},
@@ -129,7 +137,7 @@ end
 local function click(system, wx, wy, button)
     if #avaialble_actions > 0 then
         select_command(wx, wy)
-    elseif #selected_units > 0 then
+    elseif selected_unit then
         create_command_options(wx, wy)
     end
 end
